b7cca3e244d3852f2bb26e39fd340b24388f1621
ncordoves <nataliecordoves@gmail.com>
2023-03-01 21:48:29 -0500

    completed html and js

--- js/background.js
+++ js/background.js
@@ -1,24 +1,26 @@
+
+
 document.addEventListener('DOMContentLoaded', function() {
     // on click event
     // go back into body and change style of background to the random image
-    console.log('hi');
-    // const button = document.querySelector("button");
-    // button.addEventListener("click", function() {
+    const button = document.querySelector("button");
+    button.addEventListener("click", function() {
+        function getRandomInt(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min) + min); 
+        }
+    
+        let num = getRandomInt(1, 20);
+    
+        let img = document.createElement('img');
+        img.src = `images/puppy/puppy${num}.jpeg`;
+        img.style.width = '300px';
+        img.style.height = '300px';
+        document.body.appendChild(img);
+        document.body.setAttribute('style', `background: ${img}`);
         
-    //     function getRandomInt(min, max) {
-    //         min = Math.ceil(min);
-    //         max = Math.floor(max);
-    //         return Math.floor(Math.random() * (max - min) + min); 
-    //     }
-    
-    //     let num = getRandomInt(1, 20);
-    
-    //     let img = document.createElement('img');
-    //     img.src = `hackathon/images/puppy${num}.jpeg`;
-    //     document.body.appendChild(img);
-    //     document.body.setAttribute('style', 'background', `${img}`);
-    //     document.querySelector('.photo').style.display = 'block';   
-    // })
+    })
 })
 
 // function getRandomInt(min, max) {
@@ -34,3 +36,4 @@ document.addEventListener('DOMContentLoaded', function() {
 //   button.textContent = `Click count: ${event.detail}`;
 // });
   
+//`${img}`
